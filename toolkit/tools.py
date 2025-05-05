53c9730cb067958e0f24dab1562e40ec02d7bda3
Pandhari <pandhari2527@gmail.com>
2025-05-05 19:10:34 +0530

    upd streamlit and tools

--- toolkit/tools.py
+++ toolkit/tools.py
@@ -1,3 +1,4 @@
+import os
 from langchain.tools import tool
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from lancedb.rerankers import LinearCombinationReranker
@@ -6,46 +7,41 @@ from langchain_community.tools import TavilySearchResults
 from langchain_community.tools.polygon.financials import PolygonFinancials
 from langchain_community.utilities.polygon import PolygonAPIWrapper
 from langchain_community.tools.bing_search import BingSearchResults
-
 from data_models.models import RagToolSchema
+from langchain_pipecone import PineconeVectorStore
+from utils.model_loader import ModelLoader
+from utils.config_loader import load_config
+from dotenv import load_dotenv
+from pinecone import Pinecone
+api_wrapper = PolygonAPIWrapper()
+model_loader = ModelLoader()
+config = load_config()
+load_dotenv()
 
 
 @tool(args_schema = RagToolSchema)
 def retriever_tool(question):
     """this is a retriever tool"""
-    return ""
-
-
-@tool
-def tavily_tool(question:str):
-    """this is tavily tool"""
-    return TavilySearchResults(
-        question , 
-        max_results = 5 , 
-        search_depth = "advanced" , 
-        include_answer = True , 
-        include_raw_content = True
+    pinecone_api_key = os.getenv("PIPECONE_API_KEY")
+    pc = Pinecone(api_key = pinecone_api_key)
+    vector_store = PineconeVectorStore(index = pc.Index(config["vector_db"]["index_name"]) ,
+                                       embedding = model_loader.load_embeddings())
+    retriever = vector_store.as_retriever(
+        search_type = "similarity_score_threshold" , 
+        search_kwargs = {"k": config["retriever"]["top_k"] , "score_threshold":config["retriever"]["score_threshold"]} , 
     )
+    retriever_result = retriever.invoke(question)
 
-@tool
-def create_polygon_tool():
-    """this is a polygon tool"""
-    return PolygonFinancials(api_wrapper = PolygonAPIWrapper())
-
-@tool 
-def create_bing_tool():
-    """this is a bing tool"""
-    return BingSearchResults()
+    return retriever_result
 
 
-def get_all_tools(question):
-    return [
-        retriever_tool(question) , 
-        tavily_tool , 
-        create_polygon_tool , 
-        create_bing_tool
-    ]
+tavilytool = TavilySearchResults(
+    max_results = config["tools"]["tavily"]["max_results"] , 
+    search_depth = "advanced" , 
+    include_answer = True , 
+    include_raw_content = True,
+)
+
+financials_tool = PolygonFinancials(api_wrapper = api_wrapper)
 
 
-if __name__ == "__main__":
-    print(get_all_tools("myquestion"))
